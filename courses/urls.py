70284edd184f0692611f9a11164da0922eccd5e4
Yahya Dalal <yahyadalal05@gmail.com>
2026-03-22 19:04:24 +0000

    Added utl Patterns

--- courses/urls.py
+++ courses/urls.py
@@ -1,2 +1,9 @@
 from django.urls import path
-urlpatterns = []
+from .views import CourseListCreateView, CourseDetailView, CohortListCreateView, CohortDetailView
+
+urlpatterns = [
+    path('', CourseListCreateView.as_view(), name='course-list'),
+    path('<int:pk>/', CourseDetailView.as_view(), name='course-detail'),
+    path('cohorts/', CohortListCreateView.as_view(), name='cohort-list'),
+    path('cohorts/<int:pk>/', CohortDetailView.as_view(), name='cohort-detail'),
+]
